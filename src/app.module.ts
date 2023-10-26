942f8de886c0424ac6b32204f4ad0e82b6148509
francisco.pena <francisco.pena@humblebee.se>
2023-10-26 11:33:05 +0200

    Add env example and update readme

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,8 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from '@hapi/joi';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
@@ -7,7 +10,12 @@ import { CoffeesModule } from './coffees/coffees.module';
 @Module({
   imports: [
     CoffeesModule,
-    MongooseModule.forRoot('mongodb://localhost:27017/nest-course'),
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        MONGO_URI: Joi.required(),
+      }),
+    }),
+    MongooseModule.forRoot(process.env.MONGO_URI),
   ],
   controllers: [AppController],
   providers: [AppService],
