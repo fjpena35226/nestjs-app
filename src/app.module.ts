b065e32caf10f1d4f754298f783f9c2e19bc6cdc
francisco.pena <francisco.pena@humblebee.se>
2023-10-26 11:37:35 +0200

    updating readme, env validation schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from '@hapi/joi';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [CoffeesModule, PrismaModule],
+  imports: [
+    CoffeesModule,
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        DATABASE_URL: Joi.required(),
+      }),
+    }),
+    PrismaModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
